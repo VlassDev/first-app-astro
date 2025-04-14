2cf84c9bc8bd78e50ce68021b3eccb62c396c29e
Vladimir Sarmiento <sarmientovlass@gmail.com>
2025-04-14 16:20:03 -0500

    se agregó js de bootstrap para nabvar responsive

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,30 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_cbaPvjSJ.mjs';
+import { manifest } from './manifest_1vZcJtap.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/blog.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/blog.astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "296b16f7-edb4-4332-be54-3c7b5154f5a2",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_22UMrHFO.mjs';
+export { renderers } from '../renderers.mjs';
